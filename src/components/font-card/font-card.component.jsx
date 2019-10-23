854402607f8efbaaaea7047548341612e9c1f8a1
joshtru <joshuaojc@gmail.com>
2019-10-23 09:08:35 -0500

    Fixed font card to handle text overflow

--- src/components/font-card/font-card.component.jsx
+++ src/components/font-card/font-card.component.jsx
@@ -10,7 +10,14 @@ const FontText = styled.p`
   font-family: '${props => props.font || "Open Sans"}', ${props =>
   props.category || "sans-serif"};
   font-size: ${props => props.size || 32}px;
-  padding-bottom: 2rem;
+  line-height: 1.1;
+   display: block;
+  display: -webkit-box;
+      height: 141px;
+      -webkit-line-clamp: 4;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+      text-overflow: ellipsis;
 `;
 
 const FontCard = ({ family, category, size, text, cardMd, cardLg, cardXl }) => (
